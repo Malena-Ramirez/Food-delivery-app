359fbbcf6423cc780bbef9b1754f0f30ab32d281
Malena Ramirez <maramirezuribe@gmail.com>
2021-11-04 19:14:53 -0500

    Create Login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Login from './components/login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Título</h1>
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
 
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <main>
+      <h1>Login</h1>
+      <p>Logo (img)</p>
+      <p>Input group</p>
+    </main>
+  );
+};
+
+export default Login;
