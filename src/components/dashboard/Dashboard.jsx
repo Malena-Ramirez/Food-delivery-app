8402f76511a08a19a04f4d5e0b7910100fe57ba1
Malena Ramirez <maramirezuribe@gmail.com>
2021-11-04 19:51:41 -0500

    Create dashboard component

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <>
+      <header>
+        <h1>Dashboard</h1>
+        <p></p>
+        <nav>
+          <p>Tramites</p>
+        </nav>
+      </header>
+      <section>
+        <p>Timeline</p>
+      </section>
+    </>
+  );
+};
+
+export default Dashboard;
