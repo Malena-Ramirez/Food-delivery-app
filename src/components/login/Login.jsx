a2b5946c25383cc4124cd94401c9edc5f8f1f689
Malena Ramirez <maramirezuribe@gmail.com>
2021-11-05 01:15:15 -0500

    Style login form group

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { LoginMain, LogoImg, Title } from './LoginStyles';
+import {
+  LoginMain,
+  LogoImg,
+  Title,
+  LoginContainer,
+  LoginInput,
+} from './LoginStyles';
 
 const Login = () => {
   return (
     <LoginMain>
-      <Title>Login</Title>
-      <LogoImg src='https://i.imgur.com/zxhhEk9.png' alt='Logo de Rappi' />
-      <form>
-        <input type='email' name='email' id='email' placeholder='Email' />
-        <button type='submit'>Continuar</button>
-      </form>
+      <LoginContainer>
+        <Title>Login</Title>
+        <LogoImg
+          src='https://i.imgur.com/zxhhEk9.png'
+          alt='Logo de Rappi'
+          className='mb-5'
+        />
+        <form className='form-floating'>
+          <LoginInput
+            type='email'
+            className='form-control mb-4'
+            id='floatingInput'
+            placeholder='Email'
+          />
+          <label htmlFor='floatingInput'>Email</label>
+          <button type='submit' className='btn btn-dark w-100 py-2'>
+            Continuar
+          </button>
+        </form>
+      </LoginContainer>
     </LoginMain>
   );
 };
--- src/components/login/LoginStyles.js
+++ src/components/login/LoginStyles.js
@@ -4,9 +4,17 @@ export const LoginMain = styled.main`
   background: linear-gradient(45deg, #ff3689 0%, #ff284e 25%, #ff441f 90%);
   height: 100vh;
   display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
+`;
+
+export const LoginContainer = styled.div`
+  background-color:#c0c0c075;
+  padding: 5em 2.5em;
+  border-radius: 20px;
+  @media (min-width: 768px) { 
+    padding: 6.5em 15em;
+   }
 `;
 
 export const Title = styled.h1`
@@ -21,4 +29,14 @@ export const LogoImg = styled.img`
   max-height: 270px;
   min-width: 200px;
   min-height: 200px;
+`;
+
+export const LoginInput = styled.input`
+  background-color: #fdfdfd;
+  &:focus {
+    border-color: #FF441F;
+    /* box-shadow: none; */
+    /* box-shadow: 2px 2px 10px 1px rgba(255,68,31,0.6);  */
+    box-shadow: 0 0 0 .25rem #ff441f75; 
+  }
 `;
