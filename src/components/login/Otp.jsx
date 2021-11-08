cd5311871bf617f959b1cffa3fffdc4353032099
Malena Ramirez <maramirezuribe@gmail.com>
2021-11-08 17:14:20 -0500

    Update otp input

--- src/components/login/Otp.jsx
+++ src/components/login/Otp.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import OtpInput from 'react-otp-input';
 
 const Otp = () => {
+  const [code, setCode] = useState('');
+
+  const handleChange = (code) => setCode(code);
+
   return (
     <div>
-      <p>OTP/ correo válido</p>
+      <OtpInput
+        value={code}
+        onChange={handleChange}
+        numInputs={4}
+        separator={<span style={{ width: '8px' }}></span>}
+        isInputNum={true}
+        shouldAutoFocus={true}
+        inputStyle={{
+          border: '1px solid transparent',
+          borderRadius: '8px',
+          width: '54px',
+          height: '54px',
+          fontSize: '1.2rem',
+          color: '#626262',
+          fontWeight: '400',
+          caretColor: 'blue',
+        }}
+        focusStyle={{
+          border: '1px solid #CFD3DB',
+          outline: 'none',
+        }}
+        hasErrored={false}
+        errorStyle={{ color: 'red' }}
+      />
     </div>
   );
 };
