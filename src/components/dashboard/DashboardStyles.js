73c24281590cfc64fa300de3dad2b9f4e689ed7f
Malena Ramirez <maramirezuribe@gmail.com>
2021-11-08 23:09:29 -0500

    Style restaurant's logo in dashboard

--- src/components/dashboard/DashboardStyles.js
+++ src/components/dashboard/DashboardStyles.js
@@ -33,12 +33,21 @@ export const AvatarImage = styled.img`
 
 export const Restaurant = styled.li`
   list-style: none;
-  padding: 1.5em 2em;
+  padding: 1.5em;
   border-radius: 10px;
   margin-bottom: 1em;
-  background: linear-gradient(45deg, #ff3689 0%, #ff284e 25%, #ff441f 90%);
+  /* background: linear-gradient(45deg, #ff3689 0%, #ff284e 25%, #ff441f 90%); */
+  background-color: ${props => false ? '#ff441f' : '#c0c0c075'};
+  color: white;
   &:hover{
     cursor: pointer;
     filter: drop-shadow(0 0 0.75rem #AAAAAA);
   }
+`;
+
+export const RestaurantLogo = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
 `;
