7d3117fa8eb9c90ad87946d752525f0be3db1e66
Malena Ramirez <maramirezuribe@gmail.com>
2021-11-08 21:15:06 -0500

    Load restaurants and deliveries status

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deliveryAction } from './actions/deliveryAction';
+import { restaurantsAction } from './actions/restaurantsAction';
+import Dashboard from './components/dashboard/Dashboard';
 import Login from './components/login/Login';
 import GlobalStyle from './globalStyles';
 
 function App() {
+  const { logged } = useSelector(state => state.logged);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getList = (category, action) => {
+      axios.get(`http://localhost:3004/${category}`)
+        .then((res) => {
+          const list = res.data;
+          dispatch(action(list))
+        })
+    }
+    getList("restaurants", restaurantsAction);
+    getList("deliveryStatus", deliveryAction);
+
+  }, [dispatch]);
+
+
   return (
     <>
       <GlobalStyle />
-      <Login />
+      {
+        logged ? <Dashboard /> : <Login />
+      }
+
     </>
   );
 }
