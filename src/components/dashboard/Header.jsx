62de59b5994583e2f5a3e91ee10aae7fc1f43b3d
Malena Ramirez <maramirezuribe@gmail.com>
2021-11-08 23:09:52 -0500

    Render data from store

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { restaurantSelectedAction } from '../../actions/restaurantSelectedAction';
 import {
   AvatarContainer,
   AvatarImage,
   HeaderContainer,
   Restaurant,
+  RestaurantLogo,
   Title,
 } from './DashboardStyles';
 
 const Header = () => {
+  const { user } = useSelector((state) => state.user);
+  const { restaurants } = useSelector((state) => state.restaurants);
+  const { image, orders } = user;
+
+  let restaurantsOrders = [];
+  orders.forEach((element) => {
+    const match = restaurants.find(
+      (restaurant) => restaurant.id === element.restaurantID
+    );
+    restaurantsOrders.push(match);
+  });
+
+  const dispatch = useDispatch();
+
+  const handleClick = (restaurant) => {
+    dispatch(restaurantSelectedAction(restaurant));
+  };
+
   return (
     <HeaderContainer className='bg-dark'>
       <Title>Dashboard</Title>
       <AvatarContainer>
-        <AvatarImage
-          src='https://us.123rf.com/450wm/thesomeday123/thesomeday1231709/thesomeday123170900021/85622928-icono-de-perfil-de-avatar-predeterminado-marcador-de-posici%C3%B3n-de-foto-gris-vectores-de-ilustraciones.jpg?ver=6'
-          alt='Ávatar del usuario'
-        />
+        <AvatarImage src={image} alt='Ávatar del usuario' />
         <form className='d-flex input-group ms-3'>
           <span className='input-group-text' id='basic-addon1'>
             <img src='https://i.imgur.com/tB9uhDh.png' alt='Búsqueda' />
@@ -30,9 +48,18 @@ const Header = () => {
       </AvatarContainer>
       <nav className='mt-5'>
         <ul>
-          <Restaurant>Tramite 1</Restaurant>
-          <Restaurant>Tramite 2</Restaurant>
-          <Restaurant>Tramite 3</Restaurant>
+          {restaurantsOrders.map((restaurant) => (
+            <Restaurant
+              key={restaurant.id}
+              onClick={() => handleClick(restaurant)}
+            >
+              <RestaurantLogo
+                src={restaurant.image}
+                alt='Logo del restaurante'
+              />
+              {restaurant.name}
+            </Restaurant>
+          ))}
         </ul>
       </nav>
     </HeaderContainer>
