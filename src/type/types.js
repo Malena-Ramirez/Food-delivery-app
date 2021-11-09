78456e6296c644eb69bcd96f6c8912a47d943e83
Malena Ramirez <maramirezuribe@gmail.com>
2021-11-08 21:03:29 -0500

    Add restaurants and deliveries types

--- src/type/types.js
+++ src/type/types.js
@@ -1,6 +1,10 @@
 export const types = {
   user: '[Auth] user',
 
-  logged: '[Auth] logged'
+  logged: '[Auth] logged',
+
+  restaurantsLoad: '[Restaurants] Load restaurants',
+
+  deliveryStatusLoad: '[Delivery] Load delivery status',
 
 }
